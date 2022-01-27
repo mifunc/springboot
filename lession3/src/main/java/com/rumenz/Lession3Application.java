5a88b0b20b16bc8cc91ce3484d9ffef49b335ebc
nginxim <372669319@qq.com>
2022-01-27 13:44:01 +0800

    3.第三节:SpringBoot中web项目推荐目录结构.md

---
+++ lession3/src/main/java/com/rumenz/Lession3Application.java
@@ -0,0 +1,13 @@
+package com.rumenz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lession3Application.class, args);
+    }
+
+}
---
+++ lession3/src/test/java/com/rumenz/Lession3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumenz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lession3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
